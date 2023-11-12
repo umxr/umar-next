9cf678d695084b962092df9add7f33489d11ff2a
Umar <umarg1997@gmail.com>
2023-11-12 22:07:04 +0000

    added vercel anayltics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import clsx from 'clsx';
 import type { Metadata } from 'next';
 import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://umar.codes'),
@@ -57,6 +58,7 @@ export default function RootLayout({
       <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           {children}
+          <Analytics />
         </main>
       </body>
     </html>
